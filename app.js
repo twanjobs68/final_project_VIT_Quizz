fe051a1872844ed0c20ac63aecbfb87dca9ef1d0
Antwan Grant <twanjobs68@gmail.com>
2021-08-10 00:56:34 -0400

    first commit for final project VIT

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+function check() {
+    var ch=0;
+    var q6=docuement.quiz1.question6.value;
+    if(q6 = "amethyst") {
+        
+        ch++
+        document.write(ch);
+    }
+    console.log("Hello")
+}
+
