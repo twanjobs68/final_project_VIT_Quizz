a0e2acbaa2547b81b980af4d39d3c0bedcf79ee3
Antwan Grant <twanjobs68@gmail.com>
2021-08-10 22:35:21 -0400

    working on javascript function to get score

--- app.js
+++ app.js
@@ -4,27 +4,27 @@ function check() {
 
     var c=0;
     //variable for each question
-    var q1=docuement.quiz1.question1.value;
-    var q2=docuement.quiz1.question2.value;
-    var q3=docuement.quiz1.question3.value;
-    var q4=docuement.quiz1.question4.value;
-    var q5=docuement.quiz1.question5.value;
-    var q6=docuement.quiz1.question6.value;
-    var q7=docuement.quiz1.question7.value;
-    var q8=docuement.quiz1.question8.value;
-    var q9=docuement.quiz1.question9.value;
-    var q10=docuement.quiz1.question10.value;
+    var q1=docuement.quiz.question1.value;
+    var q2=docuement.quiz.question2.value;
+    var q3=docuement.quiz.question3.value;
+    var q4=docuement.quiz.question4.value;
+    var q5=docuement.quiz.question5.value;
+    // var q6=docuement.quiz.question6.value;
+    // var q7=docuement.quiz.question7.value;
+    // var q8=docuement.quiz.question8.value;
+    // var q9=docuement.quiz.question9.value;
+    // var q10=docuement.quiz.question10.value;
 
     if(q1 == "12:30pm-1pm") {c++}
     if(q2 == "Salmon") {c++}
     if(q3 == "7am") {c++}
     if(q4 == "legumes") {c++}
     if(q5 == "Spring") {c++}
-    if(q6 == "Ameythyst") {c++}
-    if(q7 == "Boss Baby") {c++}
-    if(q8 == "Venom") {c++}
-    if(q9 == "Lemons") {c++}
-    if(q10 == "Niagara Falls") {c++}
+    // if(q6 == "Ameythyst") {c++}
+    // if(q7 == "Boss Baby") {c++}
+    // if(q8 == "Venom") {c++}
+    // if(q9 == "Lemons") {c++}
+    // if(q10 == "Niagara Falls") {c++}
     
         
         
