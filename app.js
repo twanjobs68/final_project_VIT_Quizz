2ba2b5948fd4310681f047fdc50a27854095dd98
Antwan Grant <twanjobs68@gmail.com>
2021-08-10 20:34:53 -0400

    adding if statements to validate answer and add to correct count

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 
 
 function check() {
-    var ch=0;
+
+    var c=0;
+    //variable for each question
+    var q1=docuement.quiz1.question1.value;
+    var q2=docuement.quiz1.question2.value;
+    var q3=docuement.quiz1.question3.value;
+    var q4=docuement.quiz1.question4.value;
+    var q5=docuement.quiz1.question5.value;
     var q6=docuement.quiz1.question6.value;
-    if(q6 = "amethyst") {
+    var q7=docuement.quiz1.question7.value;
+    var q8=docuement.quiz1.question8.value;
+    var q9=docuement.quiz1.question9.value;
+    var q10=docuement.quiz1.question10.value;
+
+    if(q1 == "12:30pm-1pm") {c++}
+    if(q2 == "Salmon") {c++}
+    if(q3 == "7am") {c++}
+    if(q4 == "legumes") {c++}
+    if(q5 == "Spring") {c++}
+    if(q6 == "Ameythyst") {c++}
+    if(q7 == "Boss Baby") {c++}
+    if(q8 == "Venom") {c++}
+    if(q9 == "Lemons") {c++}
+    if(q10 == "Niagara Falls") {c++}
+    
         
-        ch++
-        document.write(ch);
+        
+        document.write(c);
     }
     console.log("Hello")
-}
+
 
